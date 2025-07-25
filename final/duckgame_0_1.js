6783afe2b265f28c2cd6f5251154af97b097d65e
obscured08 <obscuredpi@gmail.com>
2025-07-25 09:51:45 -0500

    added 404 page stuff

--- final/duckgame_0_1.js
+++ final/duckgame_0_1.js
@@ -300,6 +300,11 @@ app.post('/increment', (req, res) => {
     });
 });
 
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
